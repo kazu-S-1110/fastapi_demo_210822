5a06dbe25d8536a2083fc8c77c80182f0e90fa61
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-22 13:20:00 +0900

    routersのプレースホルダだけ定義。fastapiインスタンスに組み込み。swaggerで確認おk。

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from api.routers import task, done
 
 app = FastAPI()
 
@@ -6,3 +7,6 @@ app = FastAPI()
 @app.get("/hello")
 async def hello():
     return {"message": "hello, world!, how r y?"}
+
+app.include_router(task.router)
+app.include_router(done.router)

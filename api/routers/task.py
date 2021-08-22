310811d654c90cb7d8d918a4f9d9db21c0b74e9b
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-22 13:52:37 +0900

    createスキーマを定義。ちゃんと動いた！めっちゃ簡単に実装できるな♪

--- api/routers/task.py
+++ api/routers/task.py
@@ -10,9 +10,10 @@ async def list_tasks():
     return [task_schema.Task(id=1, title="first task")]
 
 
-@router.post("/tasks")
-async def create_task():
-    pass
+@router.post("/tasks", response_model=task_schema.TaskCreateResponse)
+async def create_task(task_body: task_schema.TaskCreate):
+    # **を使用してキーワード引数として展開、つまり**task_body.dict()はtitle=task_body.title, done=task_body.doneになる
+    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
 
 
 @router.put("/tasks/{task_id}")
--- api/schemas/task.py
+++ api/schemas/task.py
@@ -2,7 +2,24 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 
-class Task(BaseModel):
+class TaskBase(BaseModel):  # 共通部分をクラス化
+    title: Optional[str] = Field(None, example="I will do something.")
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskCreateResponse(TaskCreate):
     id: int
-    title: Optional[str] = Field(None, example="Complete react")
+
+    class Config:
+        orm_mode = True
+
+
+class Task(TaskBase):
+    id: int
     done: bool = Field(False, description="Complete Flag")
+
+    class Config:  # DBと接続する際に使用
+        orm_mode = True

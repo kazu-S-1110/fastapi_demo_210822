ed2c497443752c1998cd8c3cafee7ec3346c9f9f
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-23 00:07:26 +0900

    postの動作を編集（dbと連携）。ちゃんと動いた〜。マニュアルの間違いに気づけてよかった〜。

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List
 import api.schemas.task as task_schema  # modelsのものはtask_modelとして区別するために読み替えてimportする
+import api.cruds.task as task_crud
+from api.db import get_db
 
 router = APIRouter()
 
@@ -10,10 +13,15 @@ async def list_tasks():
     return [task_schema.Task(id=1, title="first task")]
 
 
+# @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
+# async def create_task(task_body: task_schema.TaskCreate):
+#     # **を使用してキーワード引数として展開、つまり**task_body.dict()はtitle=task_body.title, done=task_body.doneになる
+#     return task_schema.TaskCreateResponse(id=1, **task_body.dict())
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
-async def create_task(task_body: task_schema.TaskCreate):
-    # **を使用してキーワード引数として展開、つまり**task_body.dict()はtitle=task_body.title, done=task_body.doneになる
-    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
+async def create_task(
+    task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)
+):
+    return await task_crud.create_task(db, task_body)
 
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
